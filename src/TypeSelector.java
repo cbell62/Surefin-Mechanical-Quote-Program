8695f737cf813711314ec52ee986dd7ed4dd5913
Calista Bell <56490200+cbell62@users.noreply.github.com>
2020-09-02 12:03:36 -0400

    Add files via upload

---
+++ src/TypeSelector.java
@@ -0,0 +1,3 @@
+public enum TypeSelector {
+    sixteen_o_five, sixteen_o_eight, one_one_o_four, one_one_four_seven, d_two_six, d_two_six_two, sixteen_o_two, sixteen_twenty_two, one_one_one_two, one_one_one_three, one_one_one_five, one_one_one_six, one_one_one_seven, one_one_two_six,one_one_two_eight, one_one_two_five, one_one_two_seven, one_one_four_three, one_one_o_nine, one_one_two_four, one_one_nine_two
+}
